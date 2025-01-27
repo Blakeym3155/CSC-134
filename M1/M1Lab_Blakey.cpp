f0f31e5565c4d30b6b43d3600fc0af6626d4aae6
Blakeym3155 <114183184+Blakeym3155@users.noreply.github.com>
2025-01-27 16:32:17 +0000

    added semicolon still have issues

---
+++ M1/M1Lab_Blakey.cpp
@@ -0,0 +1,22 @@
+// CSC 134
+// M1LAB
+// Your Name
+// The Date
+#include <iostream>
+using namespace std;
+int main() {
+// This program will simulate an apple orchard.
+// The owner’s name
+    string name = “Mikayla”;
+// number of apples owned
+    int apples = 40;
+// price per apple
+    double cost_each = 0.25;
+    int total;
+// calculate the total price of the apples
+// TODO
+    apples * cost_each = total;
+// print all the information about the orchard
+// TODO
+    cout << “Welcome to “ name “’s apple orchard.” << endl;
+}
