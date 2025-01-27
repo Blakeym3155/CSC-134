b7f6eaf1cf24b3b39d71f0c0a60290f5a09ed723
Blakeym3155 <114183184+Blakeym3155@users.noreply.github.com>
2025-01-27 15:50:40 +0000

    added statement about me to M1T2

---
+++ M1T1/M1T2_Blakey.cpp
@@ -0,0 +1,21 @@
+// CSC-134
+// Hello World
+// Mikayla Blakey
+// 1/27/25
+
+
+//include user input and output
+#include <iostream>
+  
+using namespace std;
+  
+int main()
+{
+// for M1T1 add a few words about yourself
+// and things you like
+   cout << "Hello, World!" << endl;
+   cout << "My name is Mikayla Blakey" << endl;
+   cout << "I enjoy working with epoxy resin, my uv resin printer, beadwork and more to design products for my business." << endl;
+   cout<< endl;
+   return 0;
+}
