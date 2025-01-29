74003ce94b490e8198142ef83a1618df91cb36b0
Blakeym3155 <114183184+Blakeym3155@users.noreply.github.com>
2025-01-29 16:42:19 +0000

    started M1H1

---
+++ M1/M1HW1_Blakey.cpp
@@ -0,0 +1,20 @@
+// CSC 134
+// M1HW
+// Mikayla Blakey
+// 1/29/25
+
+
+#include <iostream>
+int main() {
+
+using namespace std;
+
+string movie_name; 
+int release_year;
+double gross_movie_sales;
+
+cout<< "The movie I chose is called" << movie_name << 
+
+
+return 0;
+}
