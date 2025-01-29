ec210ee3edade725aab07f2a0dcace1335058051
Blakeym3155 <114183184+Blakeym3155@users.noreply.github.com>
2025-01-29 15:35:52 +0000

    edited M1Lab comments

--- M1/M1HW_Blakey.cpp
+++ M1/M1HW_Blakey.cpp
@@ -6,19 +6,17 @@
 #include <iostream>
 using namespace std;
 int main() {
-// This program will simulate an apple orchard.
 // The owner’s name
     string name = "Mikayla";
 // number of apples owned
     int apples = 40;
 // price per apple
     double cost_each = 0.25;
+// calculate the total price of the apples
     double total = apples * cost_each;
 // calculate the total price of the apples
-// TODO
 
 // print all the information about the orchard
-// TODO
     cout << "Welcome to " << name << "'s apple orchard" << endl;
     cout << "We have " << apples << " apples for $" << cost_each << " each making your grand total $" << total << " dollars!";
     cout << endl;
--- M1/M1Lab_Blakey.cpp
+++ M1/M1Lab_Blakey.cpp
@@ -5,19 +5,16 @@
 #include <iostream>
 using namespace std;
 int main() {
-// This program will simulate an apple orchard.
 // The owner’s name
     string name = "Mikayla";
 // number of apples owned
     int apples = 40;
 // price per apple
     double cost_each = 0.25;
-    double total = apples * cost_each;
 // calculate the total price of the apples
-// TODO
-
+    double total = apples * cost_each;
 // print all the information about the orchard
-// TODO
+
     cout << "Welcome to " << name << "'s apple orchard" << endl;
     cout << "We have " << apples << " apples for $" << cost_each << " each making your grand total $" << total << " dollars!";
     cout << endl;
