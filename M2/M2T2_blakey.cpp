054044b8637590ef0e101a069ead783010b6a7b1
Blakeym3155 <114183184+Blakeym3155@users.noreply.github.com>
2025-02-03 15:41:05 +0000

    added variables and made sentences

---
+++ M2/M2T2_blakey.cpp
@@ -0,0 +1,27 @@
+// CSC 134
+// M2T2
+// Mikayla Blakey
+// 2/3/25
+
+#include  <iostream>
+#include <iomanip> //need for 2 decimals
+using namespace std;
+
+int main() {
+//set up variables
+    double food_price = 5.99;
+    string store_name = "Mikkis Snacks";
+    string snack_name = "Salted Pretzel";
+
+    cout << "Welcome to " << store_name << "!" << endl;
+    cout << "We only have one snack available, our famous " << snack_name << ". The cost is $" << food_price << " dollars." << endl;
+    cout <<  endl;
+
+
+
+
+
+
+
+return 0;
+}
