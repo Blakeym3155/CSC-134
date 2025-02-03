ff673dddbd2e3014c80c91c4ec93339af33a694e
Blakeym3155 <114183184+Blakeym3155@users.noreply.github.com>
2025-02-03 04:53:10 +0000

    added lines of text, testing is good thus far.

--- M1/M1HW1_Blakey.cpp
+++ M1/M1HW1_Blakey.cpp
@@ -9,11 +9,16 @@ int main() {
 
 using namespace std;
 
-string movie_name; 
-int release_year;
-double gross_movie_sales;
+string movie_name = "Despicable Me 2";  // Movie name
+int release_year = 2013;                // Release year
+double gross_movie_sales = 970.8 ;
 
-cout<< "The movie I chose is called" << movie_name << 
+cout<< "The movie I chose is called " << movie_name << "!" << endl;
+cout << "The company that produced this movie is Universal Pictures" << endl;
+cout << "The gross movies sales were " << movie_name << "million dollars." << endl;
+cout << endl;
+cout << endl;
+
 
 
 return 0;
