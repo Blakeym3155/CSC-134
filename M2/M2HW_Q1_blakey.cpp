77b5d5964eb4b38b916ea0ee5ba6ebffad81c9ef
Blakeym3155 <114183184+Blakeym3155@users.noreply.github.com>
2025-02-21 04:42:02 +0000

    finished input, need output and calculations

--- M2/M2HW_Q1_blakey.cpp
+++ M2/M2HW_Q1_blakey.cpp
@@ -12,11 +12,39 @@ using namespace std;
 int main()
 {
 
+    double user_balance ;
+    int account_number;
+    double deposit_amount;
+    double withdrawal_amount;
+    char user_choice;
+    string user_name;
+
+cout<< "Welcome to The Welson Brothers Bank!" << endl;
+cout << "Input the name on the account: ";
+getline(cin, user_name);
+cout<< endl;
+cout<< "Input your current account balance: ";
+cin >> user_balance;
+cout << endl;
+cout<< "Do you need to deposit or withdrawal? Type 'D' for deposit and 'W' for withdrawal: ";
+cin >> user_choice;
+cout << endl;
 
 
-
-
-
+if (user_choice == 'D'){
+    cout << "Input how much you will be depositing here: ";
+    cin >> deposit_amount;
+    cout << endl;
+}
+else if (user_choice == 'W'){
+    cout << "Input how much you will be withdrawing here: ";
+    cin >> withdrawal_amount;
+    cout << endl;
+}
+else{
+    cout << "Input Error Retry";
+    
+}
 
 return 0;
 }
