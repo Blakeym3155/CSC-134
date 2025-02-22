e5a82c50baf46bb9db68cf52281bc059eb3a23c9
Blakeym3155 <114183184+Blakeym3155@users.noreply.github.com>
2025-02-22 01:04:40 +0000

    Finished program 1 for M2HW1

--- M2/M2HW_Q1_blakey.cpp
+++ M2/M2HW_Q1_blakey.cpp
@@ -13,17 +13,18 @@ int main()
 {
 
     double user_balance ;
-    int account_number;
+    int account_number = 1056;
     double deposit_amount;
     double withdrawal_amount;
     char user_choice;
     string user_name;
+    double new_balance;
 
 cout<< "Welcome to The Welson Brothers Bank!" << endl;
 cout << "Input the name on the account: ";
 getline(cin, user_name);
 cout<< endl;
-cout<< "Input your current account balance: ";
+cout<< "Input your current account balance: $";
 cin >> user_balance;
 cout << endl;
 cout<< "Do you need to deposit or withdrawal? Type 'D' for deposit and 'W' for withdrawal: ";
@@ -32,19 +33,26 @@ cout << endl;
 
 
 if (user_choice == 'D'){
-    cout << "Input how much you will be depositing here: ";
+    cout << "Input how much you will be depositing here: $";
     cin >> deposit_amount;
+    new_balance = deposit_amount + user_balance;
     cout << endl;
 }
 else if (user_choice == 'W'){
-    cout << "Input how much you will be withdrawing here: ";
+    cout << "Input how much you will be withdrawing here: $";
     cin >> withdrawal_amount;
+    new_balance = user_balance - withdrawal_amount;
     cout << endl;
 }
 else{
-    cout << "Input Error Retry";
+    cout << "Input Error! Reset the program.";
     
 }
 
+cout << setprecision(2) << fixed;  
+cout << "Account number "<< account_number <<  " has a new balance of $" << new_balance << endl;
+cout << "Thank you come again next time!";
+cout << endl;
+cout << endl;
 return 0;
 }
