f8ebfed34b7f222de1c0fc455d0737095a7dc8db
Blakeym3155 <114183184+Blakeym3155@users.noreply.github.com>
2025-02-26 15:27:53 +0000

    started M3Lab2

---
+++ M3/M3Lab2_Blakey.cpp
@@ -0,0 +1,24 @@
+// CSC 134
+// M3Lab2
+// Mikayla Blakey
+// 2/26/25
+// create a program that converst numerical grades into letter grades!
+
+#include <iostream>
+using namespace std;
+ 
+int main(){
+
+    //Define my variables
+    double num_grade; // 0-100
+    string letter_grade ; /// "A", "B", "C"
+
+
+    cout << "This program converts your number grade into a letter";
+    cout << "Input the desired number grade here: ";
+    //find letter
+    //find number
+
+
+    return 0;
+}
