77b1ad49d62299c6a9ab58a8a0adc2f890d18b34
Blakeym3155 <114183184+Blakeym3155@users.noreply.github.com>
2025-03-24 15:42:17 +0000

    made M4HW files to edit later

---
+++ M4/M4HW_Blakey.cpp
@@ -0,0 +1,13 @@
+// CSC 134
+// M4HW
+// Mikayla Blakey
+// 3/24/25
+// I will be making a calculator like program that produces the times table of the chosen number by the user
+
+#include  <iostream>
+using namespace std; 
+
+int main() {
+
+
+}
