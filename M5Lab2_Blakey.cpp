d7db8cfb2590100afb795cf1c219249fcca8400c
Blakeym3155 <114183184+Blakeym3155@users.noreply.github.com>
2025-04-02 14:30:09 +0000

    finshed M5T1

---
+++ M5Lab2_Blakey.cpp
@@ -0,0 +1,43 @@
+// CSC 134
+// M5T1 
+// Mikayla Blakey
+// 4/2/25
+
+#include <iostream>
+using namespace std;
+
+// Declare (Write the prototypes for)
+// the getLength,
+// getWidth, getArea, and displayData
+// functions here.
+
+
+int main()
+double length, width, area;
+{
+	// This program calculates the area of a rectangle.
+	// TODO: fix any syntax errors
+	
+   double length,    // The rectangle's length
+          width,     // The rectangle's width
+          area;      // The rectangle's area
+          
+   // Get the rectangle's length.
+   length = getLength();
+   
+   // Get the rectangle's width.
+   width = getWidth()
+   
+   // Get the rectangle's area.
+   are = getArea();
+   
+   // Display the rectangle's data.
+   displayData(length, width, area);
+          
+   return 0;
+}
+
+//***************************************************
+// TODO: write the getLength, getWidth, getArea,    *
+// and displayData functions below.                 *
+//***************************************************
