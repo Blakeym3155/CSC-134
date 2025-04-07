9db5c8b2936fab8d67584ca4d63e67b5215e5671
Blakeym3155 <114183184+Blakeym3155@users.noreply.github.com>
2025-04-07 15:06:57 +0000

    finished M6T1

--- M6/M6T1_blakey.cpp
+++ M6/M6T1_blakey.cpp
@@ -3,18 +3,31 @@
 // Mikayla Blakey
 // 4/7/25
 
+#include <iostream>
+using namespace std;
 int main() {
     //first version will have no array
     const int DAYS = 5;
-    int cars_today;
-    int cars_total;
+    int cars_today ;
+    int cars_total= 0 ;
     double cars_average;
+    int cars[DAYS];
 
     cout << "Enter cars that pass for each day."<< endl;
-    for (int i=0; i< DAYS; i==){
-        cout << "Day " << i+1 << " : ";
+    for (int i=0; i< DAYS; i++) {
+        cout << "Day " << i+1 << " : " ;
         cin >> cars_today;
+        cars[i] = cars_today;
         cars_total += cars_today;
     }
     cout << "Over " << DAYS << " days, total cars = " << cars_total << endl;
+
+cars_average = cars_total / DAYS ;
+cout << "For an average of " << cars_average << " each day." << endl;
+
+// no well print an array 
+for ( int i= 0; i < DAYS; i++){
+    cout<< "Day: " << i+1 << " Count: " << cars[i] << endl;
+}
+cout << endl;
 } 
