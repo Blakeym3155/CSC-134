b5be3f69f336e8cec6b5a97b80e714978d6e3216
Blakeym3155 <114183184+Blakeym3155@users.noreply.github.com>
2025-04-07 14:42:56 +0000

    added to M6T1

---
+++ M6/M6T1_blakey.cpp
@@ -0,0 +1,19 @@
+// CSC 134
+// M6T1 
+// Mikayla Blakey
+// 4/7/25
+
+int main(){
+    //first version will have no array
+    const int DAYS = 5;
+    int cars_today;
+    int cars_total;
+    double cars_average;
+
+    cout << "Enter cars that pass for each day."<< endl;
+    for (int i=0; i< DAYS; i==){
+        cout << "Day " << i+1 << " : ";
+        cin >> cars_today;
+        cars_total += cars_today;
+    }
+}
